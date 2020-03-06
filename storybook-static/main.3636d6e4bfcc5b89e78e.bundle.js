7fb5e90fd9fb8756f4c02fc3cc28197e7d5d902a
Dani Riera <danielriera@MacBook-Pro-de-Daniel.local>
2020-03-06 12:11:13 +0100

    Add knob addon to storybook

---
+++ storybook-static/main.3636d6e4bfcc5b89e78e.bundle.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{1:function(n,o){},470:function(n,o,p){p(471),p(613),p(1260),p(1369),n.exports=p(1403)}},[[470,1,2]]]);
---
+++ storybook-static/precache-manifest.8a5b521be1e1dda342b45fea185dac2d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4b545966b746f88cc71c5e4d6a06990",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.a9752bdc03756fcdaec1.bundle.js"
+  },
+  {
+    "url": "runtime~main.a9752bdc03756fcdaec1.bundle.js"
+  },
+  {
+    "revision": "4d237722c70669d08c04",
+    "url": "static/css/main.35000dfd.chunk.css"
+  },
+  {
+    "url": "vendors~main.a9752bdc03756fcdaec1.bundle.js"
+  },
+  {
+    "url": "vendors~main.a9752bdc03756fcdaec1.bundle.js.LICENSE.txt"
+  }
+]);
